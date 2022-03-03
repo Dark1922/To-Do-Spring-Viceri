428789d8e2171a34768ef0af523fdf71576952ed
João Victor <jvdematos004@gmail.com>
2022-03-03 12:23:02 -0300

    Update UsuarioServiceImpl.java

--- to-do/src/main/java/com/viceri/todo/domain/service/impl/UsuarioServiceImpl.java
+++ to-do/src/main/java/com/viceri/todo/domain/service/impl/UsuarioServiceImpl.java
@@ -1,5 +1,7 @@
 package com.viceri.todo.domain.service.impl;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +15,7 @@ import com.viceri.todo.domain.dto.UsuarioDTO;
 import com.viceri.todo.domain.dto.input.UsuarioInput;
 import com.viceri.todo.domain.exception.NegocioException;
 import com.viceri.todo.domain.exception.UsuarioNotFoundException;
+import com.viceri.todo.domain.models.Task;
 import com.viceri.todo.domain.models.Usuario;
 import com.viceri.todo.domain.repository.UsuarioRepository;
 import com.viceri.todo.domain.service.UsuarioService;
@@ -39,9 +42,9 @@ public class UsuarioServiceImpl implements UsuarioService {
 
 		Usuario usuarioAtual = usuarioInpuDisassembler.toDomainObject(usuarioInput);
 
+		validatePassword(usuarioAtual.getPassword());
 		/* Criptografia de senha */
-		String senhaCriptografada = new BCryptPasswordEncoder().encode(usuarioAtual.getPassword());
-		usuarioAtual.setPassword(senhaCriptografada);
+		usuarioAtual.setPassword(criptografarSenha(usuarioAtual.getPassword()));
 
 		return usuarioModelAssembler.toModel(usuarioRepository.save(usuarioAtual));
 	}
@@ -64,6 +67,47 @@ public class UsuarioServiceImpl implements UsuarioService {
 	public Long getUserId() {
 		String id = SecurityContextHolder.getContext().getAuthentication().getName();
 		return usuarioRepository.findByLogin(id).getId();
+	}
+
+	@Override
+	@Transactional
+	public List<Task> findUserTask() {
+		String id = SecurityContextHolder.getContext().getAuthentication().getName();
+		return usuarioRepository.findByLogin(id).getTasks();
+	}
+
+	public static void validatePassword(String password) {
+
+		int charactersValidated = 0;
+
+		if (password.length() < 6) {
+			charactersValidated = 6 - password.length();
+		} else {
+
+			if (!password.matches("(.*)[0-9](.*)")) {
+				charactersValidated++;
+			}
+			if (!password.matches("(.*)[a-z](.*)")) {
+				charactersValidated++;
+			}
+			if (!password.matches("(.*)[A-Z](.*)")) {
+				charactersValidated++;
+			}
+			if (!password.matches("(.*)[!@#$%^&*()-+](.*)")) {
+				charactersValidated++;
+			}
+		}
+
+		if (charactersValidated > 0) {
+			throw new NegocioException(String.format("Senha precisa de ao menos um caracteres especial !@$..,"
+					+ "um número 0-9" + ", uma letra maíuscula A-Z e uma minuscula a-z , e no minimo 6 digitos"));
+		}
+	}
+
+	private String criptografarSenha(String senha) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		return encoder.encode(senha);
+
 	}
 
 }
